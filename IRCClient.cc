ebbb9d1f1772db7c6d9107c0c49efca2c65489c1
Tushar <sharm173@data.cs.purdue.edu>
2015-04-27 01:39:50 -0400

    Make IRCServer

--- IRCClient.cc
+++ IRCClient.cc
@@ -351,7 +351,7 @@ void  on_changed(GtkWidget *widget, gpointer label)
    // gtk_label_set_text(GTK_LABEL(label), value);
    	
 	room = strdup(value);
-	printf(room);
+	printf("%s",room);
 	 g_free(value);
   }
 
